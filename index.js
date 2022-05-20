a3decebd6f5fbb541b25af6ced75179cebe3d0b1
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-05-21 04:17:49 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, Intents } = require('discord.js');
+const ms = require('ms');
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS]
+});
+
+client.on('ready', async () => {
+    console.log('ギルドコマンド削除開始');
+
+    await client.guilds.fetch();
+    client.guilds.cache.forEach(async guild => {
+        await guild.commands.fetch();
+        guild.commands.cache.forEach(async command => {
+            await command.delete();
+        });
+    });
+});
+
+client.on('rateLimit', rateLimitData => {
+    console.log(`レート制限\n時刻: ${Date.now()}\nHTTPメゾット: ${rateLimitData.method}\nHTTPリクエストルート: ${rateLimitData.route}\nHTTPリクエストパス${rateLimitData.path}\nタイムアウト: ${ms(rateLimitData.timeout)}\nリクエスト最大数: ${rateLimitData.limit}\nグローバル: ${rateLimitData.global ? 'はい' : 'いいえ'}`)
+});
+
+client.login(process.argv[2]);
